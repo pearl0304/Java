a93123b681444044de2dee1d2e6b46ed2394e183
pearl0304 <81275596+pearl0304@users.noreply.github.com>
2021-04-26 11:52:16 +0900

    Java 실습

---
+++ 2020_12_23/src/메소드사용.java
@@ -0,0 +1,98 @@
+
+public class 메소드사용 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	
+System.out.print(100);		
+System.out.print(" ");	
+System.out.print(200);	
+System.out.println();
+
+System.out.printf("%d %d %d",100,200,300);	
+System.out.println();
+System.out.printf("%d %d %f",100,200,3.14);	
+System.out.printf("\n");
+System.out.printf("%d %d %d %d %d",1,2,3,4,5);
+
+		
+System.out.println();
+System.out.printf("100+100");
+System.out.printf("\n");
+System.out.printf("%d", 100+100);
+System.out.printf("\n");
+System.out.printf("%d", 300);
+System.out.println();
+System.out.printf("%f", 3.14);
+
+System.out.println();
+System.out.printf("%d %d %d %d %d %d %d", 1,2,3,4,5,6,7);
+System.out.println();
+System.out.printf("%f %f %f %f %f", 1.00,2.00,3.00,4.00,5.00);
+System.out.println();
+System.out.printf("%d %d %f %f %f", 1,2,3.00,4.00,5.00);
+System.out.println();
+
+System.out.printf("%d + %d = %d",100,200,300);
+System.out.printf("\n");
+System.out.printf("%d / %d = %f",100,200,0.5);
+System.out.printf("\n");
+
+
+System.out.printf("%d + %d = %d",100,200,300);
+System.out.printf("\n");
+
+int a = 400; 
+int b = 200; 
+
+System.out.printf("%d + %d = %d",a,b,a+b);
+System.out.printf("\n");
+
+ a = 1;
+ b = 1; 
+
+ 		 
+System.out.printf("%d * %d = %d \n",a,b,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+
+a = 2;
+b = 1; 
+
+		 
+System.out.printf("%d * %d = %d \n",a,b,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+System.out.printf("%d * %d = %d \n",a,b=b+1,a*b);
+
+
+System.out.printf("%x",16);
+System.out.printf("%c",'A');
+System.out.printf("%s-------------!!!!!!!!!!!%s","안녕하세요","반갑습니다.");
+System.out.printf("%b \n",10);
+System.out.printf("%b",null);
+
+
+
+	}
+	
+
+}
+	
+
+
---
+++ 2020_12_23/src/메소드사용1.java
@@ -0,0 +1,139 @@
+
+public class 메소드사용1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+System.out.printf("\n줄바꿈\n연습 \n");
+System.out.printf("\t탭키\n연습 \n");
+System.out.printf("이것을 \r 덮어씁니다 \n");
+
+
+System.out.printf("%d\n",123);
+System.out.printf("%5d\n",123);
+System.out.printf("%05d\n",123);
+System.out.printf("%f\n",123.45);
+System.out.printf("%7.1f\n",123.45);
+System.out.printf("%7.3f\n",123.45);
+System.out.printf("%s\n","Hi-java");
+System.out.printf("%10s\n","Hi-java");
+
+int x;
+float Y;
+
+
+x= (int) 123.45;
+y= 200;
+
+System.out.printf("x의 값 = = > %d \n"x);
+System.out.printf("y의 값 = = > %f \n",y);
+
+
+int a, b, c, d;
+
+a = 100+100;
+b = a+100;
+c = a+b-100;
+d= a+b+c;
+
+System.out.printf("a, b, c, d 의 값 ==> %d, %d, &d, %d\n",a, b, c, d);
+
+a=b=c=d=100;
+System.out.printf("a, b, c, d 의 값 ==> %d, %d, &d, %d\n",a, b, c, d);
+
+a= 100;
+a = a + 200;
+System.out.printf("a의 값 ==> %d\n",a);
+
+int y= 0xa;
+System.out.println(y);
+
+
+int a=100, b=200;
+double result; 
+
+result = (double) a/b;
+
+System.out.printf("%f \n", result);
+
+		
+	float a = 0.1234567890123456789012345f;
+	double b = 0.1234567890123456789012345;
+	
+	System.out.println(a);
+	System.out.println(b);
+
+
+
+	}
+
+}
+
+		
+		char a,b,c,d,e;
+		
+		a = 'A';
+		System.out.printf("%c \t",a);
+		System.out.printf("%d \n",(int)a);
+		
+		b = 'a';
+		System.out.printf("%c \t",b);
+		System.out.printf("%d \n",(int)b);
+		
+		c= (char)(b+1);
+		System.out.printf("%c \t",b);
+		System.out.printf("%c \n",c);
+		
+		d= 90;
+		System.out.printf("%c \n",d);
+		
+		e= '가';
+		System.out.printf("%d \t",(int)e);
+		System.out.printf("%c \n",e);
+		
+	
+	int a, b;
+	char c, d;
+	
+	a= 0xF90A;
+	b= 0x50;
+	
+	System.out.printf("%c \n",a);
+	
+	c= (char)b;
+	System.out.printf("%c \n",c);
+
+		
+		boolean boo1, boo2;
+		
+		boo1 = true;
+		System.out.printf("%s \n", boo1);
+		
+		boo2 = false;
+		System.out.printf("%s \n", boo2);
+
+String str1 = "IT CookBook 입니다";
+String str2 = "10";
+String str3 = "20";
+
+str1 = "Java 입니다";
+
+System.out.printf("%s \n", str1);
+System.out.printf("%s \n", str2 + str3);
+	*/
+
+
+		
+	
+		
+	}
+
+}	
+		
+		
+		
+		
+		
+		
+		
+		
